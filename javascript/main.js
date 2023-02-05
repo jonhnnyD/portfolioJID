0a8816526f71620d29ee7a49d4e60303447add7a
JohnnyD <jonathanisraeldonnet@gmail.com>
2023-02-05 02:08:05 +0100

    more style css-skills-funtional download cv-java script funtion

--- javascript/main.js
+++ javascript/main.js
@@ -24,4 +24,23 @@ function linkAction (){
   const navMenu = document.getElementById('nav-menu')
   navMenu.classList.remove('show-menu');
 }
-navLink.forEach(n => n.addEventListener('click', linkAction))
+navLink.forEach(n => n.addEventListener('click', linkAction));
+
+
+//================mostrar o ocultar skills===================//
+
+const skillsContenido = document.getElementsByClassName('skills__contenido')
+      skillsHeader = document.querySelectorAll('.skills__header');
+
+function barraSkills(){
+  let itemClass = this.parentNode.className;
+
+  for (i = 0; i < skillsContenido.length; i++){
+    skillsContenido[i].className = 'skills__contenido skills__close';
+  }
+  if(itemClass === 'skills__contenido skills__close'){
+    this.parentNode.className = 'skills__contenido skills__open';
+  }
+}
+
+skillsHeader.forEach((el) => el.addEventListener('click', barraSkills))
